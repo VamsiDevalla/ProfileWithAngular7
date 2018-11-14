31a7b8553002b49d517f9e50b7f2229089efdccf
vdeval113 <vamsi_devalla@comcast.com>
2018-11-14 11:00:42 -0500

    created contact component with basic curd operations

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {Component} from '@angular/core';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        ContactsComponent
       ],
     }).compileComponents();
   }));
@@ -13,15 +15,14 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+ 
+  
 });
+
+@Component({
+  selector: 'app-contacts',
+  template: '<div></div>',
+})
+class ContactsComponent {
+  constructor() {}
+}
